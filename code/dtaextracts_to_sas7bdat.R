49482446f3160e62085cb41c5116fa25f97b5c45
Ben Zipperer <benzipperer@gmail.com>
2018-05-14 13:44:35 -0400

    correct SAS archive permissions

--- code/dtaextracts_to_sas7bdat.R
+++ code/dtaextracts_to_sas7bdat.R
@@ -15,14 +15,14 @@ mcoptions <- list(preschedule=FALSE)
 
 filelistorg <- Sys.glob("extracts/epi_cpsorg_*.dta.zip") %>%
   basename() %>%
-  sub("^([^.]*).*", "\\1", .) %>% 
-  expand.grid(filename=.) %>% 
+  sub("^([^.]*).*", "\\1", .) %>%
+  expand.grid(filename=.) %>%
   mutate(id="org")
 
 filelistbasic <- Sys.glob("extracts/epi_cpsbasic_*.dta.zip") %>%
   basename() %>%
-  sub("^([^.]*).*", "\\1", .) %>% 
-  expand.grid(filename=.) %>% 
+  sub("^([^.]*).*", "\\1", .) %>%
+  expand.grid(filename=.) %>%
   mutate(id="basic")
 
 filelist <- bind_rows(filelistorg,filelistbasic)
@@ -39,14 +39,14 @@ myfunction <- function(x) {
   finalfilename <- paste0(sasfilename,".zip")
   rawdest <- file.path(origpath,origfilename)
   finaldest <- file.path("/data/cps",datasource,"epiextracts",finalfilename)
-  
+
   system(paste("pigz -dc",rawdest,">",dtafilename))
   write_sas(read_dta(dtafilename,encoding="latin1"),sasfilename)
 	system(paste("chmod 0444",sasfilename))
   system(paste("pigz -Kf",sasfilename))
+	system(paste("chmod 0444",finalfilename))
   system(paste("mv -f",finalfilename,finaldest))
   file.remove(dtafilename)
 }
 
 foreach(i=iter(filelist, by="row"), .options.multicore=mcoptions) %dopar% myfunction(i)
-
