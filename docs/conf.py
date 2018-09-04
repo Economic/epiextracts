3888f12baf0567b4a3b2aaf688b7b2f3322604bb
Ben Zipperer <benzipperer@gmail.com>
2018-09-04 12:28:54 -0400

    remove recommonmark option that was causing trouble for links

--- docs/conf.py
+++ docs/conf.py
@@ -188,7 +188,6 @@ source_suffix = ['.rst', '.md']
 # At the bottom of conf.py
 def setup(app):
     app.add_config_value('recommonmark_config', {
-            'url_resolver': lambda url: github_doc_root + url,
             'auto_toc_tree_section': 'Contents',
             }, True)
     app.add_transform(AutoStructify)
